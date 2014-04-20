13ff8033ad0d378c4441dbc943962bff7c1ce76d
akinLiu <hengxin196@gmail.com>
2014-04-20 23:06:22 +0800

    add property

--- UIView+FrameHelper.h
+++ UIView+FrameHelper.h
@@ -10,4 +10,54 @@
 
 @interface UIView (FrameHelper)
 
+/**
+ * frame.origin.x
+ */
+@property (nonatomic) CGFloat x;
+
+/**
+ * frame.origin.y
+ */
+@property (nonatomic) CGFloat y;
+
+/**
+ * frame.size.width;
+ */
+@property (nonatomic) CGFloat width;
+
+/**
+ *  frame.size.height
+ */
+@property (nonatomic) CGFloat height;
+
+/**
+ *  center.x
+ */
+@property (nonatomic) CGFloat xCenter;
+
+/**
+ *  center.y
+ */
+@property (nonatomic) CGFloat yCenter;
+
+/**
+ *  frame.origin
+ */
+@property (nonatomic) CGPoint origin;
+
+/**
+ *  frame.size
+ */
+@property (nonatomic) CGSize size;
+
+/**
+ * CGRectGetMaxX
+ */
+@property (nonatomic, readonly) CGFloat maxX;
+
+/**
+ *  CGRectGetMaxY
+ */
+@property (nonatomic, readonly) CGFloat maxY;
+
 @end
